753458c2c609d2f31d7e2b55f5f38751378fd684
Amalie <amalie.andreasen99@hotmail.com>
2025-01-03 14:09:34 +0100

    fep, anmdeldelser og fb sektion sat op

---
+++ src/assets/components/FocusedEntryPoints.vue
@@ -0,0 +1,72 @@
+<script setup>
+import TheButton from './TheButton.vue';
+import { defineProps, ref } from 'vue';
+
+defineProps({
+  image: {
+    type: String,
+    required: true
+  },
+  h3: {
+    type: String,
+    required: true
+  },
+  p: {
+    type: String,
+    required: true
+  },
+  buttonTitle: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<template>
+    <section class="focusedEntryPoints">
+        <article class="entryPoint">
+           <div class="entryPointContent" :style="{ backgroundImage: 'url(' + image + ')' }">
+            <h3>{{ h3 }}</h3>
+            <p>{{ p }}</p>
+            <TheButton :title="buttonTitle"/>
+           </div> 
+           <div class="Overlay"></div>
+        </article>
+    </section>
+</template>
+
+<style scoped>
+.entryPointContent{
+    background-image:url(../img/forsideDesktop/fepSelskabspakker.png);
+    box-shadow: inset 0 0 0 2000px rgba(0, 0, 0, 0.3);
+    height: 400px;
+    background-size: cover;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    flex-direction: column;
+    padding: 1rem;
+    text-align: center;
+    z-index: 1;
+}
+
+.entryPoint{
+    padding:2rem;
+    margin: 2rem;
+    border: 1px solid var(--gold);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+}
+
+.Overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5); 
+    z-index: 0; 
+}
+</style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,8 +1,12 @@
 <script setup>
 import {ref} from 'vue'; 
+import { Splide, SplideSlide } from '@splidejs/vue-splide';
+import '@splidejs/splide/dist/css/splide.min.css';
+
 import Navigation from '../assets/components/Navigation.vue';
 import TheButton from '../assets/components/TheButton.vue';
 import ImageSlider from '../assets/components/ImageSlider.vue';
+import FocusedEntryPoints from '@/assets/components/FocusedEntryPoints.vue';
 
 import slide1 from '../assets/img/forsideDesktop/slide1.png';
 import slide2 from '../assets/img/forsideDesktop/slide2.png';
@@ -25,6 +29,25 @@ const images =[
     {src: slide7, alt:'slide7'},
 ];
 
+const focusedEntryPointsBackground1 = "/src/assets/img/forsideDesktop/fepSelskabsmenu.png";
+const focusedEntryPointsBackground2 = "/src/assets/img/forsideDesktop/fepSelskabslokaler.png";
+const focusedEntryPointsBackground3 = "/src/assets/img/forsideDesktop/fepSelskabspakker.png";
+
+const splideOptions = ref({
+  autoplay: true,
+  slidesToScroll:1,
+  gap:50,
+  focus:'left',
+  rewind: true,
+  perPage: 3,  
+  breakpoints: {
+    768: {
+      perPage: 1, 
+    },
+  },
+});
+
+
 </script>
 
 <template>
@@ -35,12 +58,17 @@ const images =[
         <h1>{{ h1 }}</h1>
     </section>
     <section class="firstSection">
+        <div class="familienContent">
         <h2>Vi skaber unikke oplevelser - hver dag</h2>
         <p>Siden 1988 har SanGiovanni været en familiedrevet restaurant med fokus på autentisk italiensk mad og gæstfrihed. Grundlagt af Benthe og Giovanni Volpi, er restauranten vokset gennem generationer og tilbyder i dag både Trattoria & Pizzeria, den italienske bar “il Bar,” selskabslokaler og vinkælderen La Cantina. Med rødder i italienske traditioner og en passion for kvalitet, skaber vi unikke oplevelser for vores gæster – hver dag.
         </p>
+    </div>
+        <div class="familienbillede">
         <img src="../assets/img/forsideMobil/familien.png" alt="Familen Volpi" srcset="../assets/img/forsideDesktop/familien.png 768w">
+    </div>
     </section>
     <section class="secondSection">
+        <div class="canneloniContent">
         <h2>italienske delikatesser</h2>
         <p>Vores menu er skabt igennem mange år. En menu med de bedste italienske retter, hvor du ikke går ned på kvaliteten. Hver ret er kælet for. Prøv vores pizzaer med hjemmelavet koldhævet pizzadej eller en autentisk pastaret hvor du kan være sikker på at pastaen er hjemmelavet. Hver dag forbereder vi vores pasta i kælderen så den er frisklavet til dig.
         </p>
@@ -49,14 +77,132 @@ const images =[
             <TheButton title="Se menukort"/>
             <TheButton title="Book Bord"/>
         </div>
+    </div>
+        <div class="canneloniBillede">
         <img src="../assets/img/forsideMobil/canneloni.png" alt="Familen Volpi" srcset="../assets/img/forsideDesktop/canneloni.png 768w">
+    </div>
     </section>
     <div class="imageslider">
         <ImageSlider :images="images"/>
     </div>
-    <section class="focusedEntryPoints">
-        
-    </section>
+    <section class="focusedWrapper">
+    <FocusedEntryPoints
+    :image=focusedEntryPointsBackground1
+    h3="Selskabsmenu"
+    p="Skal du holde et selskab derhjemme, har vi den helt rigtige løsning for dig med vores selskabsmenu ud af huset."
+    buttonTitle="Læs mere"
+    />
+    <FocusedEntryPoints
+    :image=focusedEntryPointsBackground2
+    h3="Se vores selskabslokaler"
+    p="Vi har adskillige flotte selskabslokaler til dit arrangement. Læs mere her og se billeder fra vores La Cantina."
+    buttonTitle="Læs mere"
+    />
+    <FocusedEntryPoints
+    :image=focusedEntryPointsBackground3
+    h3="Selskabspakker & møder"
+    p="Vi tilbyder skrædersyet selskabspakker til dit næste møde eller arrangement. Mad, drikke og lokale samlet et sted."
+    buttonTitle="Læs mere"
+    />
+</section>
+<section class="reviews">
+    <h2>Hvad siger vores kunder</h2>
+    <Splide :options="splideOptions">
+    <SplideSlide >
+        <h3>Simon Løvstrup</h3>
+        <div class="stars">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+        </div>
+        <p>kompromisløs kvalitet fra enden til anden. Vi har netop afholdt forlovelsesfest i kælderen hos Sangiovanni. Fantastiske omgivelser, betjening og ikke mindst maden. <br>Alle gæster følte sig mere end velkommen og havde en super aften. Vi er måske ikke de nemmeste i verden at overraske eller imponere med god service og restaurantoplevelser. Men her blev vi sat helt til vægs. Vi kommer med sikkerhed igen!</p>
+        <div class="googleicon">
+        <img src="../assets/img/SVG/google.svg" alt="google">
+    </div>
+    </SplideSlide>
+    <SplideSlide >
+        <h3>Stine Mark Christansen</h3>
+        <div class="stars">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+        </div>
+        <p>Vi holdte vores bryllup hos San Giovanni i deres selskabslokaler med tilstødende barområde.<br> Der var god service fra start til slut; både under planlægningen og under selve festen.Lækker mad - og rigeligt af den - opmærksomme tjenere, der var gode til at fylde op i glassene under middagen.<br>Vi har absolut ikke fortrudt, at vi valgte San Giovanni og kan varmt anbefale dem til selskaber </p>
+        <div class="googleicon">
+        <img src="../assets/img/SVG/google.svg" alt="google">
+    </div>
+    </SplideSlide>
+    <SplideSlide >
+        <h3>Jonas Østergaard</h3>
+        <div class="stars">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+        </div>
+        <p>Vi var på Giovanni og havde en sublim oplevelse. Betjeningen var super og maden var fantastisk. <br>Vinene spillede rigtigt godt sammen med de serverede retter ( vi vil anbefale jer at rådføre jer med tjeneren, da han har rigtig godt styr på at sætte vin og mad sammen). <br>Prisleje og niveau stemmer rigtig fint overens  Vi vil helt klart vælge San Giovanni igen næste gang vi er i Ålborg. </p>
+            <div class="googleicon">
+        <img src="../assets/img/SVG/google.svg" alt="google">
+    </div>
+    </SplideSlide>
+    <SplideSlide >
+        <h3>Rene Kronborg</h3>
+        <div class="stars">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+        </div>
+        <p>Sødt og venligt personale ved modtagelsen. Vi var 7 som havde bestilt bord Hurtigt fik vi bestilt mad og drikkevare. Super gode vin Rød og Rosé. Vi bestilt pizza og pastaret. Super lækker og velsmagende pizza Men det ville være godt hvis pizza var skåret ud i 6 eller 8 stykker. Pasta ret var også lækker I alt en dejlig oplevelse Med mad lavet af gode råvare </p>
+            <div class="googleicon">
+        <img src="../assets/img/SVG/google.svg" alt="google">
+    </div>
+    </SplideSlide>
+    <SplideSlide >
+        <h3>Dan Lorenzen</h3>
+        <div class="stars">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+        </div>
+        <p>Super atmosfære som virkelig formår at fange den italienske krogstil, alt fra dekor til den fantastiske Ternet dug og olivenolie som pryder dit bord når du først sætter dig. Personalet er meget kompetent og venligt. Maden er exceptionel vi bestilte antipasto efterfulgt af en frisk velkomstdrink, derefter en frisk fanget lange velakompanieret af veltillavet tilbehør som man selv vælger. En dejlig let og smagsfuldt dessert sluttede aften af, alt sammen skyllet ned med rigtig god drue. Så alt i alt kan der kun gives 5 stjerner. Det er ikke billigt men det er alle pengene værd. Det anbefales at bestille bord, men det er ikke så mærkeligt. Super super. </p>
+            <div class="googleicon">
+        <img src="../assets/img/SVG/google.svg" alt="google">
+    </div>
+    </SplideSlide>
+    <SplideSlide >
+        <h3>Thomas Christensen</h3>
+        <div class="stars">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+            <img src="../assets/img/SVG/stjerne.svg" alt="stjerne">
+        </div>
+        <p>Fantastisk dag.......... Vi holdte min søns konfirmation i San Giovanni ´s selskabslokaler som er placeret i kælderen af restauranten. Rammerne var sat. En inspiration til en middelalder borg. Vi kunne se og mærke at alle hvad gjort sig ekstra umage for at vi fik en dejlig dag. Maden var sindssyg god og vinene var ligeså. Vi var så heldige, at tjeneren som præsenterede maden og vinen lige havde vundet en sølvmedalje i skels, hvilke gjorde at stemningen blev løfter med den udførlige indføring i hvad vi skulle have at spise og drikke. Alt i alt så er vi super glade for at have valgt netop San Giovanni som ramme for vores søns konfirmation. Tusinde tak for mad. </p>
+            <div class="googleicon">
+        <img src="../assets/img/SVG/google.svg" alt="google">
+    </div>
+    </SplideSlide>
+  </Splide>
+</section>
+<article class="facebookSection">
+<h2>Vi er på Facebook</h2>
+<p>Vi opdaterer ofte vores sociale medier med nyheder, inspiration til dit næste selskab og meget mere. </p>
+<div class="posts">
+    <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fsangiovanniaalborg%2Fposts%2Fpfbid0N8cAkaZY3uxe8i1HdMnEVEudYCCBueycSKhwi5rPQ72syhBBQ5XrcB5w8QYrjvnJl&show_text=true&width=500" width="300" height="300" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+    <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fsangiovanniaalborg%2Fposts%2Fpfbid02kPoDcjXta6queYkbdzEGFDbpB2dBuik6MoKJYAiBZvVtBuAPznNVBnPuDSnFGgNNl&show_text=true&width=500" width="300" height="300" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+    <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fsangiovanniaalborg%2Fposts%2Fpfbid0GEZXF4NWEfP8wqFUtuTWubvwCzENvYqiqVYLtMae23kBc4oSFDNegT7z1jtioposl&show_text=false&width=500" width="300" height="300" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+</div>
+</article>
 </template>
 
 <style scoped>
@@ -110,5 +256,72 @@ const images =[
     width: 100%;
 }
 
+.reviews{
+    background-color: var(--green);
+    padding: 2rem;
+}
 
+.reviews h2{
+    text-align: center;
+    margin-bottom: 2rem;
+}
+
+.stars{
+    display: flex;
+    gap: 0.5rem;
+    margin-block: 0.5rem;
+}
+
+.googleicon img{
+    width: 30px;
+    margin-top: 1rem;
+}
+
+.posts{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+}
+
+.facebookSection h2{
+    margin-top: 2rem;
+    text-align: center;
+}
+
+.facebookSection p{
+    margin-top: 1rem;
+    text-align: center;
+    margin-bottom: 1rem;
+}
+
+@media all and (min-width: 768px){
+.firstSection{
+    display: flex;
+}
+
+.familienContent, .canneloniContent{
+    width: 100%;
+}
+
+.secondSection{
+    display: flex;
+    flex-direction: row-reverse;
+}
+
+.familienbillede, .canneloniBillede, .menuButtons{
+    margin: 2rem;
+}
+
+.focusedWrapper{
+    display: flex;
+}
+
+.posts{
+   display: flex;
+   flex-direction: row;
+   margin-bottom: 2rem;
+}
+}
 </style>
